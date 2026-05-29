010161ff1a36cd21ba5642a7ae534fdd1025fde8
Lemchams <chamslemamou@gmail.com>
2026-05-29 19:50:56 +0000

    Start landing page basic layout

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
     return (
         <div>
-            <Button size="lg" variant="destructive">Click me</Button>
+            <p>This is a marketing page</p>
         </div>
     )
 }
